4819a51459b999ebf1c0f92d907a28813c2cd296
Tom Van Looy <tom@ctors.net>
2013-03-16 16:43:45 +0100

    fix permissions

--- php/manifests/fpm.pp
+++ php/manifests/fpm.pp
@@ -20,6 +20,23 @@ define php::fpm {
             fail("This PHP version is not supported")
         }
     }
+    
+    # Change user
+    exec { "FpmUserChange" :
+        command => "sed -i 's/user = www-data/user = vagrant/' /etc/php5/fpm/pool.d/www.conf",
+        onlyif  => "grep -c 'user = www-data' /etc/php5/fpm/pool.d/www.conf",
+        require => Package["php5-fpm"],
+        notify  => Service["php5-fpm"],
+    }
+
+    # Change group
+    exec { "FpmGroupChange" :
+        command => "sed -i 's/group = www-data/group = vagrant/' /etc/php5/fpm/pool.d/www.conf",
+        onlyif  => "grep -c 'group = www-data' /etc/php5/fpm/pool.d/www.conf",
+        require => Package["php5-fpm"],
+        notify  => Service["php5-fpm"],
+    }
+
 
     php::ini { 'fpm' : }
 }
